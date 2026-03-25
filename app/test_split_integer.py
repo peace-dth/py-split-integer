8ea458245dcc5cb665ef0ba30ad48cc61f94b32b
Nikita <nikita.polischukov@gmail.com>
2026-03-25 20:45:14 +0100

    Solution1

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,8 +2,7 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    result = split_integer(20, 6)
-    assert sum(result) == 20
+    assert sum(split_integer(20, 6)) == 20
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -15,23 +14,19 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(17, 4)
-    assert result == sorted(result)
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(3, 5)
-    assert result == [0, 0, 1, 1, 1]
+    assert split_integer(3, 5) == [0, 0, 1, 1, 1]
 
 
 def test_length_is_correct() -> None:
-    result = split_integer(20, 6)
-    assert len(result) == 6
+    assert len(split_integer(20, 6)) == 6
 
 
 def test_difference_between_parts() -> None:
-    result = split_integer(20, 6)
-    assert max(result) - min(result) <= 1
+    assert max(split_integer(20, 6)) - min(split_integer(20, 6)) <= 1
 
 
 def test_when_parts_equal_value() -> None:
